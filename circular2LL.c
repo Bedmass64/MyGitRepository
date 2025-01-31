09937a062514ccff93cac73d90b24ed545909e08
Bedmass64 <52615963+Bedmass64@users.noreply.github.com>
2025-01-31 15:01:34 -0400

    Added node deletion code

--- circular2LL.c
+++ circular2LL.c
@@ -98,3 +98,150 @@ void addNode(CircularDoublyLinkedListP list, int value){
 }
 
 
+//F2 implementation
+//purpose is to add a node at a given spot inside of the existing list.
+void addNodePosition(CircularDoublyLinkedListP list, int value, int position){
+    NodeTP newNode = (NodeTP)malloc(sizeof(NodeT));
+
+    //There is some similarity in the code between F1 and F2, as they are both
+    //doing a very similar job, but their job as a function as a whole has a 
+    //different objective.
+    if (newNode == NULL){
+        printf("Memory allocation failure\n");
+        //exit failure to terminate program, since return will end the function
+        exit(EXIT_FAILURE);
+        //return;
+    }
+    //setting the currentValue of newNode to value.
+    newNode->currentValue = value;
+    //Adding a node using the addNode if it is the first element in the list.
+    if (list->firstValue == NULL){
+        addNode(list, value);
+    }
+    //This is dealing for the case where the user input is 0
+    else if(position == 0){
+        NodeTP currentNode = list->firstValue;
+
+        //inserts a new node at the start of the list.
+        list->firstValue = newNode;
+
+        //setting the pointer for nextValue of newNode to currentNode
+        newNode->nextValue = currentNode;
+
+        //since newNode is the first element now, the previous is a Null
+        newNode->prevValue = NULL;
+
+        //This goes in unison with the second pointer operation in this
+        //if statement, where currentNode's previous is set to newNode.
+        currentNode->prevValue = newNode;
+
+    }
+    //dealing for all other valid input included in the list.
+    else{
+        NodeTP currentNode = list->firstValue;
+        //This is formatting the for loop to walk and check for every value
+        //until the end of the list is reached (NULL)
+        for (int i = 0; i < (position -1); i++){
+            currentNode = currentNode->nextValue;
+            //this is the check to see if it is trying to replace the position
+            //of one of the null terminators
+            if (currentNode == NULL){
+                //If this check is passed, the malloc will be freed and the
+                //pointer will be nullified, and since the \0 can only be found
+                //on the start or end of the list, then we know that we have 
+                //arrived at the end of the list and can return this function.
+                printf("Invalid position, unable to add your node.\n");
+                free(newNode);
+                newNode = NULL;
+                return;
+            }
+        }
+        //Attributing newNodes pointer to the next value equal to the current
+        //Node's next value
+        newNode->nextValue = currentNode->nextValue;
+        //After the new node has been put in the correct place to attribute its
+        //value for currentNode pointing to next value, the previous value is set
+        //to the currentNode itself.
+        newNode->prevValue = currentNode;
+        //specifying what the surrounding for currentNode is and checking
+        //to see if it is not the last element in the list.
+        //updating the currentNode's previous value to be set to the newNode
+        if (currentNode->nextValue != NULL){
+            currentNode->nextValue->prevValue = newNode;
+        }
+        //updating the currentNode's next value to be set to the newNode
+        currentNode->nextValue = newNode;
+
+        //Check to see if the new node is the first item to be swapped in
+        if (newNode->prevValue == NULL){
+            list->firstValue = newNode;
+        }
+        //Check to see if the new node is the last item to be swapped in
+        if (newNode->nextValue == NULL){
+            list->lastValue = newNode;
+        }
+        printf("Node with the value %d added at the position %d in your list.\n", value, position);
+    }
+    //inrementing the counter for the amount of elements that are currently in the list.
+    (list->elementCount)++;
+}
+
+void deleteNode(CircularDoublyLinkedListP list, int value){
+    //Checking to see if the list is empty
+    if (list->firstValue == NULL){
+        printf("The list is empty, No elements can be deleted.\n");
+        return;
+    } 
+    else{
+        NodeTP currentNode = list->firstValue;
+
+        while(currentNode != NULL){
+            //checking to see if the currenValue located in the node is the same
+            //when compared to that of value, meaning the element is found.
+            if (currentNode->currentValue == value){
+                //Multiple if blocks in order to make sure all conditions are read
+                //instead of using else if's
+
+                //checking for the first item in the list
+                if (currentNode->prevValue == NULL){
+                    //handling deleting the first item in the list.
+                    list->firstValue = currentNode->nextValue;
+                }
+                //Checking for the last item in the list
+                if (currentNode->nextValue == NULL){
+                    //handling deleting the last item in the list.
+                    list->lastValue = currentNode->prevValue;
+                }
+                //checking to make sure it is within the scope of the list
+                if (currentNode->prevValue != NULL){
+                    //attributing the pointer for current node to the correct
+                    //values
+                   currentNode->prevValue->nextValue = currentNode->nextValue;  
+                }
+                //checking to make sure it is within the scope of the list
+                if (currentNode->nextValue != NULL){
+                    //attributing the pointer for current node to the correct
+                    //values
+                    currentNode->nextValue->prevValue = currentNode->prevValue;
+                }
+                //Freeing up the currentNode and nulling out its positions.
+                currentNode->nextValue = NULL;
+                currentNode->prevValue = NULL;
+                //freeing up the currentNode
+                free(currentNode);
+                currentNode = NULL;
+                printf("Node with value of %d appearing first in the list has been deleted\n", value);
+                //decrementing the element count found in list used for checks inside of main.
+                (list->elementCount)--;
+                return;
+            }
+            else{
+                //This is cycling the value of currentNode for the while loop
+                currentNode = currentNode->nextValue;
+            }
+        } 
+        //outside of the while loop, meaning the value was not found
+        printf("Node with value %d was not found as an element in the list\n", value);
+    }
+}
+
